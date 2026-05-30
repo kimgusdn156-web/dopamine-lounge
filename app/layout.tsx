4d163278daf11a0a9716e0398de0cac6706b42a3
kimgusdn156-web <kimgusdn156@gmail.com>
2026-05-30 22:06:12 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css'
 const geist = Geist({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-   title: 'TEST12345',
+   title: 'TEST12345'
   description: '창원 상남동 프라이빗 라운지 DOPAMINE',
   verification: {
     google: 'LDwYM7oVAEBKoyeylK2ww0wGyjd0gwQcB4XgPqfvwZQ',
